f6d33403a0b78d54c5ae25b4c25e2ebe5b9d0e1d
Taohit Abdul <taohit2021@gmail.com>
2024-10-26 16:08:55 +0100

    changed comments

--- RubberDucky-JumpScare/JumpScare.ps1
+++ RubberDucky-JumpScare/JumpScare.ps1
@@ -1,35 +1,4 @@
-############################################################################################################################################################                      
-#                                  |  ___                           _           _              _             #              ,d88b.d88b                     #                                 
-# Title        : JumpScare         | |_ _|   __ _   _ __ ___       | |   __ _  | | __   ___   | |__    _   _ #              88888888888                    #           
-# Author       : I am Jakoby       |  | |   / _` | | '_ ` _ \   _  | |  / _` | | |/ /  / _ \  | '_ \  | | | |#              `Y8888888Y'                    #           
-# Version      : 1.0               |  | |  | (_| | | | | | | | | |_| | | (_| | |   <  | (_) | | |_) | | |_| |#               `Y888Y'                       #
-# Category     : Prank             | |___|  \__,_| |_| |_| |_|  \___/   \__,_| |_|\_\  \___/  |_.__/   \__, |#                 `Y'                         #
-# Target       : Windows 10,11     |                                                                   |___/ #           /\/|_      __/\\                  #     
-# Mode         : HID               |                                                           |\__/,|   (`\ #          /    -\    /-   ~\                 #             
-#                                  |  My crime is that of curiosity                            |_ _  |.--.) )#          \    = Y =T_ =   /                 #      
-#                                  |   and yea curiosity killed the cat                        ( T   )     / #   Luther  )==*(`     `) ~ \   Hobo          #                                                                                              
-#                                  |    but satisfaction brought him back                     (((^_(((/(((_/ #          /     \     /     \                #    
-#__________________________________|_________________________________________________________________________#          |     |     ) ~   (                #
-#                                                                                                            #         /       \   /     ~ \               #
-#  github.com/I-Am-Jakoby                                                                                    #         \       /   \~     ~/               #         
-#  twitter.com/I_Am_Jakoby                                                                                   #   /\_/\_/\__  _/_/\_/\__~__/_/\_/\_/\_/\_/\_#                     
-#  instagram.com/i_am_jakoby                                                                                 #  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |#              
-#  youtube.com/c/IamJakoby                                                                                   #  |  |  |  |( (  |  |  |  \\ |  |  |  |  |  |#
-############################################################################################################################################################
-
-<#
-.NOTES
-	This script can be run as is with the provided execution file
-.DESCRIPTION 
-	This script will download a scary image and a scream sound effect hosted with this payload and host volume will be raised to max level
-	Upon running this script it will immediately pause after the downloads until a mouse movement is detected 
-	The capslock button will be pressed every 3 seconds to prevent sleep, and act as an indicator the payload is ready 
-	After a mouse movement is detected their wallpaper will change to the scary image provided and the scream sound effect will play
-#>
-
-############################################################################################################################################################
-
-# Download Image; replace link to $image to add your own image
+# Download Image
 
 $image =  "https://github.com/Ta0hit/FlipperZero/blob/main/RubberDucky-JumpScare/jumpscare.png"
 
@@ -38,7 +7,7 @@ iwr $i -O $env:TMP\i.png
 
 iwr https://github.com/Ta0hit/FlipperZero/blob/main/RubberDucky-JumpScare/jumpscare.png?dl=1 -O $env:TMP\i.png
 
-# Download WAV file; replace link to $wav to add your own sound
+# Download WAV file
 
 $wav = "https://github.com/I-Am-Jakoby/hak5-submissions/blob/main/OMG/Payloads/OMG-JumpScare/female_scream.wav?raw=true"
 
@@ -52,7 +21,7 @@ iwr $w -O $env:TMP\s.wav
 <#
 
 .NOTES 
-	This will take the image you downloaded and set it as the targets wall paper
+	This will take the image you downloaded and set it as your wallpaper
 #>
 
 Function Set-WallPaper {
