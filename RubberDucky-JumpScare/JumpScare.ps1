e5e622b22673c62d121f0e63a4988ed4b6a3331b
Taohit Abdul <taohit2021@gmail.com>
2024-10-27 01:50:44 +0100

    feature - select between random images

--- RubberDucky-JumpScare/JumpScare.ps1
+++ RubberDucky-JumpScare/JumpScare.ps1
@@ -7,10 +7,18 @@ iwr $c -O $env:TMP\c.png
 iwr https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/cat.png?dl=1 -O $env:TMP\c.png
 
 ## Jumpscare
-$image =  "https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/jumpscare.png"
+<#$image =  "https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/jumpscare.png"
 $i = -join($image,"?dl=1")
 iwr $i -O $env:TMP\i.png
 iwr https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/jumpscare.png?dl=1 -O $env:TMP\i.png
+#>
+$images = @(
+    "https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/jumpscare.png?dl=1",
+    "https://raw.githubusercontent.com/Ta0hit/FlipperZero/refs/heads/main/RubberDucky-JumpScare/jumpscare2.png?dl=1",
+)
+$random = Get-Random -Minimum 0 -Maximum $images.Count
+$i = -join($images[$random],"?dl=1")
+iwr $i -O $env:TMP\i.png 
 
 # Download WAV file
 $wav = "https://github.com/Ta0hit/FlipperZero/blob/main/RubberDucky-JumpScare/female_scream.wav?raw=true"
@@ -182,6 +190,6 @@ $caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
 
 #If true, toggle CapsLock key, to ensure that the script doesn't fail
 if ($caps -eq $true) {
-$key = New-Object -ComObject WScript.Shell
-$key.SendKeys('{CapsLock}')
+    $key = New-Object -ComObject WScript.Shell
+    $key.SendKeys('{CapsLock}')
 }
